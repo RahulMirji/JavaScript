48b03ea8f14807e41ab23c8b4f5f52a565fcd7bc
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-06-16 23:01:21 +0530

    Todo html js course

---
+++ Projects/Todo/todo.js
@@ -0,0 +1,16 @@
+let todoList = [];
+
+function  addTodo(){
+  let inputElement = document.querySelector('#todo-input');
+  let todoItem = inputElement.value;
+  todoList.push(todoItem);
+  inputElement.value = '';
+  
+}
+
+function displayItems() {
+  let displayElement = document.querySelector('#todo-items');
+  for(let i=0; i<todoList.length; i++)
+    displayElement.innerText = displayElement.innerText + todolist[i];
+     
+} 
