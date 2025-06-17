87096409c507fc7b10a81c1aee7365733bc42452
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-06-17 22:07:19 +0530

    todo going on with js

--- Projects/Todo/todo.js
+++ Projects/Todo/todo.js
@@ -1,16 +1,21 @@
 let todoList = [];
+displayItems()
 
 function  addTodo(){
   let inputElement = document.querySelector('#todo-input');
   let todoItem = inputElement.value;
   todoList.push(todoItem);
   inputElement.value = '';
+  displayItems()
   
 }
 
 function displayItems() {
-  let displayElement = document.querySelector('#todo-items');
+  let containerElement = document.querySelector('.item-container');
+
+  let newHTML
+  displayElement.innerText = " ";
   for(let i=0; i<todoList.length; i++)
-    displayElement.innerText = displayElement.innerText + todolist[i];
+    displayElement.innerText = displayElement.innerText + "\n" + todoList[i];
      
 } 
