8b511b27a77c4668f260d4e4a3c3aee7934dface
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-06-18 21:23:36 +0530

    todo completed

--- Projects/Todo/todo.js
+++ Projects/Todo/todo.js
@@ -1,21 +1,42 @@
-let todoList = [];
+let todoList = [
+  {
+    item:'Buy milk', 
+    dueDate: '4/04/2025'
+  },
+    {item:'college',
+    dueDate:'4/04/2025'
+    }
+];
 displayItems()
 
 function  addTodo(){
   let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
-  todoList.push(todoItem);
+  let todoDate = dateElement.value;
+  todoList.push({item:todoItem,dueDate:todoDate});
   inputElement.value = '';
+  dateElement.value = '';
   displayItems()
   
 }
 
 function displayItems() {
-  let containerElement = document.querySelector('.item-container');
+  let containerElement = document.querySelector('.todo-container');
 
-  let newHTML
-  displayElement.innerText = " ";
-  for(let i=0; i<todoList.length; i++)
-    displayElement.innerText = displayElement.innerText + "\n" + todoList[i];
+  let newHTML = '';
+
+  for(let i=0; i<todoList.length; i++){
+    // let item = todoList[i].item;
+    // let dueDate = todoList[i].dueDate;
+    let {item, dueDate} = todoList[i];
+    newHTML+=`
+  
+    <span>${item}</span>
+    <span>${dueDate}</span>   
+    <button class = "btn-delete" onclick ="todoList.splice(${i},1); displayItems();">Delete</button>
+    `;
+  }
+  containerElement.innerHTML = newHTML;
      
 } 
